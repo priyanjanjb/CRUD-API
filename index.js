ec2736495cf585a8761e188891aeaa0cfbadea41
priyanjan perera <84628100+priyanjanjb@users.noreply.github.com>
2024-01-05 19:38:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const router = require("./router");
-const router = require("./router");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
